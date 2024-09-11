6cb95a3d1e04cd57f92861b76b5b20898a5c5de2
Robert Ahlin <rob@ahlin.org>
2024-09-11 14:37:11 +0200

    Add contact form

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -46,3 +46,24 @@ window.onclick = function(event) {
         }
     });
 }
+
+// Send message from contact form
+function sendMail(contactForm) {
+    emailjs.init("sKLGmElmQrqhdKkiF");
+    emailjs.send("service_ipclfkf", "i4-solutions-ab", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "message": contactForm.messagelines.value
+    })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+                alert(`Thank you ${contactForm.name.value}. Email has been sent successfully. I will reply as soon as possible to ${contactForm.emailaddress.value}!`);
+            },
+            function (error) {
+                console.log("FAILED", error);
+                alert("Failed to send email. Please try again later.");
+            }
+        );
+    return false;  // To block from loading a new page
+}
---
+++ assets/js/sendEmail.js
@@ -0,0 +1,21 @@
+//assets/js/sendEmail.js
+// Send message from contact form
+function sendMail(contactForm) {
+    emailjs.init("sKLGmElmQrqhdKkiF");
+    emailjs.send("service_ipclfkf", "rahlin_cv-site", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "message": contactForm.messagelines.value
+    })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+                alert(`Thank you ${contactForm.name.value}. Email has been sent successfully. I will reply as soon as possible to ${contactForm.emailaddress.value}!`);
+            },
+            function (error) {
+                console.log("FAILED", error);
+                alert("Failed to send email. Please try again later.");
+            }
+        );
+    return false;  // To block from loading a new page
+}
