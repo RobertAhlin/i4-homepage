605ffb84e22888570b3007cbe1f76b9d9adb20c8
Robert Ahlin <rob@ahlin.org>
2024-09-11 14:56:45 +0200

    Redmade the email script and form

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -4,13 +4,13 @@ function sendMail(contactForm) {
     emailjs.init("sKLGmElmQrqhdKkiF");
     emailjs.send("service_ipclfkf", "rahlin_cv-site", {
         "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
+        "from_email": contactForm.email.value,
         "message": contactForm.messagelines.value
     })
         .then(
             function (response) {
                 console.log("SUCCESS", response);
-                alert(`Thank you ${contactForm.name.value}. Email has been sent successfully. I will reply as soon as possible to ${contactForm.emailaddress.value}!`);
+                alert(`Thank you ${contactForm.name.value}. Email has been sent successfully. I will reply as soon as possible to ${contactForm.email.value}!`);
             },
             function (error) {
                 console.log("FAILED", error);
